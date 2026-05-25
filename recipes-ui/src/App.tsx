486f21381872414d28739cc3ed4ccbf9da91da07
phil kaasa <phil@kaaphi.com>
2026-05-25 10:42:50 -0700

    UI WIP : Add mantine, app shell and get logout working.

--- recipes-ui/src/App.tsx
+++ recipes-ui/src/App.tsx
@@ -1,43 +1,80 @@
-import './App.css'
+// import './App.css'
 import { useAuth } from 'react-oidc-context'
+import '@mantine/core/styles.css';
+import { Button, MantineProvider, Title } from '@mantine/core';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
 
 
 function App() {
   const auth = useAuth();
+   const [opened, { toggle }] = useDisclosure();
 
   const signOutRedirect = () => {
     const clientId = import.meta.env.VITE_OAUTH_CLIENT_ID;
-    const logoutUri = "<logout uri>";
+    const logoutUri = `${window.location.origin}/`;
     const domain = import.meta.env.VITE_OAUTH_DOMAIN;
     window.location.href = `https://${domain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
+  const AppInternal = () => {
+    if (auth.isLoading) {
+      return <div>Loading...</div>;
+    }
 
-  if (auth.error) {
-    return <div>Encountering error... {auth.error.message}</div>;
-  }
+    if (auth.error) {
+      return <div>Encountering error... {auth.error.message}</div>;
+    }
 
-  if (auth.isAuthenticated) {
+    if (auth.isAuthenticated) {
+      return (
+        <div>
+          <pre> Hello: {auth.user?.profile.email} </pre>
+          <pre> ID Token: {auth.user?.id_token} </pre>
+          <pre> Access Token: {auth.user?.access_token} </pre>
+          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+          <Button onClick={() => auth.removeUser()}>Sign out</Button>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-        <button onClick={() => auth.removeUser()}>Sign out</button>
+        <Button onClick={() => auth.signinRedirect()}>Sign in</Button>
+        <Button onClick={() => signOutRedirect()}>Sign out</Button>
       </div>
-    );
+  );
   }
 
   return (
-    <div>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
-    </div>
+    <MantineProvider>
+      <AppShell
+        padding="md"
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+      >
+        <AppShell.Header>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            hiddenFrom="sm"
+            size="sm"
+          />
+
+          <Title order={1}>Recipes</Title>
+        </AppShell.Header>
+
+        <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+        <AppShell.Main><AppInternal /></AppShell.Main>
+      </AppShell>
+    </MantineProvider>
   );
 }
 
--- recipes-ui/vite.config.ts
+++ recipes-ui/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Your backend server URL
+        changeOrigin: true,              // Ensures the request appears to come from the frontend server
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: removes '/api' prefix before forwarding
+      },
+    },
+  },
 })
